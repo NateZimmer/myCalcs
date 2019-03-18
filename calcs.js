99eeb276c0fb646c0429c06c147afc8295aca097
Nathan Zimmerman <nathan.m.zimmerman@jci.com>
2019-03-18 17:02:58 -0500

    .

---
+++ calcs.js
@@ -0,0 +1,24 @@
+
+
+
+var txPwr = 0;
+var txGain = 0;
+var rxGain = 0;
+var rxPwrSen = 0; 
+var pathLoss = 0;
+
+
+
+function onLoad(){
+	document.getElementById("myBtn").addEventListener("click", displayDate);
+}
+
+
+function calcOutput(){
+	
+}
+
+
+function calculateRange(){
+
+}
